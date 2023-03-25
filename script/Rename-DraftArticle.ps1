a238d47a15fedbbcf6336b13879996c7495697e9
Dave Carroll <powershell@anovelidea.org>
2023-03-25 14:05:12 -0500

    more work on rename drafts script

--- script/Rename-DraftArticle.ps1
+++ script/Rename-DraftArticle.ps1
@@ -81,12 +81,13 @@ if ($DraftArticles.Count -gt 0) {
 foreach ($Article in $DraftArticles) {
     $FrontMatter = Get-Content -Path $Article.FullName -Raw | ConvertFrom-Yaml -ErrorAction Ignore
     if ($FrontMatter.ContainsKey('date')) {
-        '{0}: DATE : {1}' -f $FrontMatter['title'],$FrontMatter['date'].ToShortDateString()
-        if ($FrontMatter['date'].ToShortDateString() -eq $CurrentDate.ToShortDateString()) {
+        $ArticleDate = [datetime]::Parse($FrontMatter['date']).ToShortDateString()
+        '{0}: DATE : {1}' -f $FrontMatter['title'],$ArticleDate
+        if ($ArticleDate -eq $CurrentDate.ToShortDateString()) {
             $RenameFileList.Add($Article)
             '{0}: Including article to rename.' -f $FrontMatter['title']
         } else {
-            if ($FrontMatter['date'] -gt $CurrentDate) {
+            if ($ArticleDate -gt $CurrentDate) {
                 '{0}: Article is scheduled for a future date. SKIPPED' -f $FrontMatter['title']
             } else {
                 '::warning:: {0}: Article ''date'' is set in the past. Please update the ''date'' value to a future date. SKIPPED' -f $FrontMatter['title']
